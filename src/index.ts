9170c9b79111e7c49840c179cb8947b4652d6045
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2026-01-31 14:37:30 +0330

    fix(API): prevent race condition by setting and releasing lock in increment API

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,32 @@ const port = 3000;
 async function increment() {
     const value = await read();
     const newValue = value + 1;
-    return write(newValue);
+    return await write(newValue);
 }
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.post("/increment", function (req, res, next) {
+let lock = Promise.resolve();
+
+app.post("/increment", async function (req, res, next) {
+    const prev = lock;
+    let release!: () => void;
+
+    // lock the current request and await for previous request to finish
+    lock = new Promise<void>(resolve => (release = resolve));
+    await prev;
+
     increment()
         .then((result) => {
             res.json({result});
         })
         .catch((error) => {
             next(error);
-        });
+        }).finally(() => {
+        release();
+    });
 });
 
 app.listen(port, () => {
